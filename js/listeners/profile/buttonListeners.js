5f6e6334d06161171a6b9620598e34ac7715c246
MaazAhmadDeveloper <139908710+MaazAhmadDeveloper@users.noreply.github.com>
2025-12-14 12:09:19 +0500

    profile nav handled

--- js/listeners/profile/buttonListeners.js
+++ js/listeners/profile/buttonListeners.js
@@ -6,7 +6,7 @@ export function myProfileListener() {
 
   if (myProfileBtn) {
     myProfileBtn.addEventListener("click", () => {
-      window.location.href = "/profile";
+      window.location.href = "/profile/";
     });
   }
 }
